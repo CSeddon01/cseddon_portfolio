5b12a1af22bfb25adfc541e97f4ba0ac73b33a45
Christine Seddon <noddes@gmail.com>
2022-05-13 16:58:22 -0500

    updated contact form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Contact from './components/Contact';
 import ContactForm from './components/ContactForm';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
@@ -13,7 +12,7 @@ export default function App() {
   const [pages] = useState([
     { name: 'Home', description: 'About' },
     { name: 'Portfolio', description: 'Portfolio' },
-    { name: 'Contact', description: 'Contact' },
+    { name: 'ContactForm', description: 'ContactForm' },
     { name: 'Resume', description: 'Resume' },
   ]);
   const [currentPage, setcurrentPage] = useState(pages[0]);
@@ -32,11 +31,8 @@ export default function App() {
             (<Portfolio></Portfolio>)
             : currentPage.name === 'Resume' ?
               (<Resume></Resume>)
-              : currentPage.name === 'Contact' ?
-                (<div className='flex-row space-between'>
-                  (<Contact></Contact>)
-                  <ContactForm />
-                </div>)
+              : currentPage.name === 'ContactForm' ?
+                  (<ContactForm />)
                 : (<></>)
         }
       </main>
